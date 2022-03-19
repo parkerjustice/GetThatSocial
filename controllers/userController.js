74c70aca20e78c2c9d63252567b7b553b26ffecb
parkerjustice <you@example.com>
2022-03-19 10:38:44 -0400

    added js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,17 @@
 const { User, Thought } = require("../models")
+
+const userController = {
+    getAllUser (
+        req, res) { 
+        User.findAll({})
+        .populate({
+            path:"thoughts",
+            select:":)"
+        })
+        .populate({
+            path:"friends",
+            select:":)"
+        })
+
+    }
+}
