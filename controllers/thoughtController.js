722e1239352be865f0732ede713a0c5fc508de8e
parkerjustice <you@example.com>
2022-03-19 10:29:59 -0400

    added js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -9,6 +9,13 @@ const thoughtController = {
 .catch(err => {console.log (err); res.sendStatus(450);
 });
 },
+
+//removing a thought?
+removingThought({ params }, res) {
+    Thought.findOneAndDelete ({idid: params.thoughtID})
+    .then(UserDataDB => res.json(UserDataDB))
+.catch(err => res.json(err));
+},
 //same as before just minor id changes 
 getThoughtByID({ params},res){
     Thought.find({idid: params.thoughtID})
@@ -19,7 +26,21 @@ getThoughtByID({ params},res){
 });
 },
 
+//adding a reaction // needs more work
 
+addingReaction({params, body}, res){
+    Thought.create(body)
+    .then( ({idid}) => {
+        return User.findOneAndUpdate(
+            {idid: params.thoughtID},
+            {$add: {reaction: body} },
+            {new: true}
+        )
+    })
+    .then(UserDataDB => res.json(UserDataDB))
+.catch(err => {console.log (err); res.sendStatus(450); });
+
+}
 
 
 
