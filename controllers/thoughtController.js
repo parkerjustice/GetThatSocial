05d91a9498d37ea16289b8a17c99652c6a1d74e2
parkerjustice <you@example.com>
2022-03-19 10:11:27 -0400

    added js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,5 +2,11 @@ const { User, Thought } = require("../models")
 // beginning of the controllers initial setup 
 const thoughtController = { 
     getAllThoughts(req, res)
-
+{ Thought.find({})
+.populate({path: 'reactions', select: ':)'})
+.select(":)")
+.then(UserDataDB => res.json(UserDataDB))
+.catch(err => {console.log (err); res.sendStatus(450);
+});
+}
 }
