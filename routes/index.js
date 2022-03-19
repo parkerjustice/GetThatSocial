f3abd5b7dfe08f549d1c4d376c884f4e2093603d
parkerjustice <you@example.com>
2022-03-19 12:04:04 -0400

    added fun text 404

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 const apiRoute = require("./apis");
 const routes = require("express").Router();
 router.use('./apis', apiRoute);
+router.use ((req,res)=> {
+    res.status(404).send("Page Not Found Try Again")
+})
 module.exports= routes;
