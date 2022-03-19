1fd9a9785b0fb832a816660aa0a36644f47a0834
parkerjustice <you@example.com>
2022-03-19 12:40:32 -0400

    added js

--- routes/apis/users-routes.js
+++ routes/apis/users-routes.js
@@ -1,5 +1,14 @@
 const routes = require("exppress").Router();
-const { get } = require("..");
+const { get, route } = require("..");
 const {getAllUser,removingUser, getUserByID,addingFriends  } = require("../../controllers/userController");
 routes.route("/")
 .get(getAllUser);
+
+routes.route("/:ID")
+.get(getUserByID)
+.remove(removingUser);
+
+routes.route("/:usersID/friends/:friendsID")
+.post(addingFriends);
+module.exports = routes;
+//routing to the user ID
